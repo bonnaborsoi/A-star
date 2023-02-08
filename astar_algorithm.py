ab29accae67708eeda4a3784133d83c7374a5382
Bonna Borsoi <86304672+bonnaborsoi@users.noreply.github.com>
2023-02-08 00:07:42 -0300

    Versão final

--- astar_algorithm.py
+++ astar_algorithm.py
@@ -1,24 +1,13 @@
 def reconstruct_path(closed_list, current, partida):
   total_path = []
-  #print(closed_list)
-
   total_path.append(current)
 
   while current != partida:
     total_path.append(closed_list[current])
     current = closed_list[current]
-  #total_path.append(partida)
+  print("Rota encontrada: ")
   for i in range (0,len(total_path),1):
     print(total_path[len(total_path)- 1 - i])
-
-
-
-  #while current in closed_list:
-   # current = closed_list[current]
-   # total_path.prepend(current)
-  #print(total_path)
-  #print('foi')
-  #return total_path
 
 def failure():
   print("Não há caminho possível")
@@ -40,23 +29,6 @@ distancia_linha_reta = {
     14: {1: 29.8, 2: 21.8, 3: 16.6, 4: 15.4, 5: 17.9, 6: 18.2, 7: 15.6, 8: 27.6, 9: 26.6, 10: 21.2,  11: 35.5, 12: 33.6, 13: 5.1,  14: 0},    # Partindo da Estação E14
     
 }
-
-#distancia_linha_reta = [
-#        [0,    10,   18.5, 24.8, 36.4, 38.8, 35.8, 25.4, 17.6, 9.1,  16.7, 27.3, 27.6, 29.8], # Estação E1
-#        [10,   0,    8.5,  14.8, 26.6, 29.1, 26.1, 17.3, 10,   3.5,  15.5, 20.9, 19.1, 21.8], # Estação E2
-#        [18.5, 8.5,  0,    6.3,  18.2, 20.6, 17.6, 13.6, 9.4,  10.3, 19.5, 19.1, 12.1, 16.6], # Estação E3
-#        [24.8, 14.8, 6.3,  0,    12,   14.4, 11.5, 12.4, 12.6, 16.7, 23.6, 18.6, 10.6, 15.4], # Estação E4
-#        [36.4, 26.6, 18.2, 12,   0,    3,    2.4,  19.4, 23.3, 28.2, 34.2, 24.8, 14.5, 17.9], # Estação E5
-#        [38.8, 29.1, 20.6, 14.4, 3,    0,    3.3,  22.3, 25.7, 30.3, 36.7, 27.6, 15.2, 18.2], # Estação E6
-#        [35.8, 26.1, 17.6, 11.5, 2.4,  3.3,  0,    20,   23,   27.3, 34.2, 25.7, 12.4, 15.6], # Estação E7
-#        [25.4, 17.3, 13.6, 12.4, 19.4, 22.3, 20,   0,    8.2,  20.3, 16.1, 6.4,  22.7, 27.6], # Estação E8
-#        [17.6, 10,   9.4,  12.6, 23.3, 25.7, 23,   8.2,  0,    13.5, 11.2, 10.9, 21.2, 26.6], # Estação E9
-#        [9.1,  3.5,  10.3, 16.7, 28.2, 30.3, 27.3, 20.3, 13.5, 0,    17.6, 24.2, 18.7, 21.2], # Estação E10
-#        [16.7, 15.5, 19.5, 23.6, 34.2, 36.7, 34.2, 16.1, 11.2, 17.6, 0,    14.2, 31.5, 35.5], # Estação E11
-#        [27.3, 20.9, 19.1, 18.6, 24.8, 27.6, 25.7, 6.4,  10.9, 24.2, 14.2, 0,    28.8, 33.6], # Estação E12
-#        [27.6, 19.1, 12.1, 10.6, 14.5, 15.2, 12.4, 22.7, 21.2, 18.7, 31.5, 28.8, 0,    5.1],  # Estação E13
-#        [29.8, 21.8, 16.6, 15.4, 17.9, 18.2, 15.6, 27.6, 26.6, 21.2, 35.5, 33.6, 5.1,  0]     # Estação E14
-#]
 
 distancia_real = {
 #   e : [(n, d, l), (n', d', l')]
@@ -107,7 +79,6 @@ def AStar(partida, destino,linha):
     open_list.append(partida) #Acrescentando o nó de partida
 
     closed_list = {} # Para cada nó "n", closed_list[n] é o nó que imediatamente o precedeu no caminho mais "barato" a partir do início
-                     # closed_list[n] = (estacao, linha), tupla com estação da qual partiu e linha pela qual chegou
 
     # Valor somado das distâncias até o momento
     g_score = {} # Para cada nó "n", g_score[n] é o custo do caminho mais barato do início até n que se conhece atualmente
@@ -115,55 +86,39 @@ def AStar(partida, destino,linha):
         g_score[i] = float('inf')
     g_score[partida] = 0 # A distância da estação inicial até ela mesma é 0
 
-    f_score = {} # Para cada nó "n", f_score[n] é o custo do nó calculado da seguinte maneira: f_score[n] = g_score[n] + heuristic_table[n - 1][destino - 1]
+    f_score = {} # Para cada nó "n", f_score[n] é o custo do nó calculado da seguinte maneira: f_score[n] = g_score[n] + heuristic_table[n][destino]
     for i in range(1,15,1):
         f_score[i] = float('inf')
     f_score[partida] = heuristic_table[partida][destino]
   
     while len(open_list) > 0: 
-        current = None
-        # O nódulo com menor valor de f, inicializado como "None"
-        #####current_winner = None
-
+        current = None # O nódulo com menor valor de f, inicializado como "None"
+        
         for no_inexplorado in open_list:
-            # índices na matriz de heurística
-            #####estacao_winner = current_winner[0] - 1 
-            #####estacao_destino = destino[0] - 1
-            #####estacao_atual = v[0] - 1
-
-            #print(f'f_score[no_inexplorado] = {f_score[no_inexplorado]} e f_score[current] {f_score[current]}')
             if current == None or f_score[no_inexplorado] < f_score[current]:
                 current = no_inexplorado;
-                print(f'Nó atual: {current}')
 
-        # Alterar isso daqui pra o negativo e colocar o for dentro do if 
+        print(f'Fronteira: {open_list}')
+
         # Se n é o nó de destino
         if (current == destino):
-            print(f'Done!')
-            # Print do tempo aqui
-            #print(f'Tempo final: {g_score[partida] + heuristic_table[partida][destino]} minutos')
-            #print(f'Tempo final: {f_score[partida]} minutos')
+            print(f'Feito!\n')
+            print(f'Tempo final: {f_score[destino]} minutos')
             return reconstruct_path(closed_list, current, partida) # função de reconstruir o caminho do nó de partida até o nó de chegada
 
         open_list.remove(current)
-        
-        #####closed_list[current_winner[0]] = .
-        #####closed_list.append(current_winner)
-
-        # Número da estação atual
-        #####estacao_current = current_winner[0]
 
         # Checar os vizinhos
         for (estacao_vizinho, tempo_vizinho, linha_vizinho) in get_neighbors(current):
           tentative_g_score = g_score[current] + tempo_vizinho
           if tentative_g_score < g_score[estacao_vizinho]:
             closed_list[estacao_vizinho] = current
-            #print(closed_list)
 
             g_score[estacao_vizinho] = tentative_g_score
-            f_score[estacao_vizinho] = tentative_g_score + heuristic_table[estacao_vizinho - 1][destino - 1]
+            f_score[estacao_vizinho] = tentative_g_score + heuristic_table[estacao_vizinho][destino]
             if linha != linha_vizinho:
               g_score[estacao_vizinho] += 4
+              linha = linha_vizinho
             if estacao_vizinho not in open_list:
               open_list.append(estacao_vizinho)
 
@@ -171,6 +126,10 @@ def AStar(partida, destino,linha):
 
 def main():
   linha = 'azul'
-  AStar(6,13, linha)
+  partida = 1
+  destino = 14
+
+  # Chamada da função
+  AStar(partida, destino, linha)
 
 main()
